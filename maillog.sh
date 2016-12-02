f3e394941d5a35deb2b2c8f56e417c9073e94212
AdapterShack <jeff.robertson@gmail.com>
2016-12-01 23:12:15 -0500

    rewrite to use zip and mailx

--- maillog.sh
+++ maillog.sh
@@ -1,22 +1,20 @@
 #!/bin/sh
-# Credits: http://unix.stackexchange.com/questions/102092/mail-send-email-with-attachment-from-commandline
-v_mailpart="$(uuidgen)"
-echo "To: $2
-Subject: $1
-Content-Type: multipart/mixed; boundary=\"$v_mailpart\"
-MIME-Version: 1.0
 
-This is a multi-part message in MIME format.
---$v_mailpart
-Content-Type: text/html
-Content-Disposition: inline
+if [ $# != 2 ]; then
+        echo "Usage: $0 <file> <email address>"
+else
 
-<html><body>$1 is attached</body></html>
+        tmpdir="/tmp/$(uuidgen)"
 
---$v_mailpart
-Content-Transfer-Encoding: base64
-Content-Type: application/octet-stream; name=$1
-Content-Disposition: attachment; filename=$1
+        mkdir $tmpdir
 
-`base64 $1`
---$v_mailpart--" | /usr/sbin/sendmail -t
+        zipfile="$tmpdir/$1.zip"
+
+        zip -9 $zipfile $1
+
+        echo "$1 is attached" | mailx -s "Sending: $1" -a $zipfile $2
+
+        rm $zipfile
+
+        rmdir $tmpdir
+fi
